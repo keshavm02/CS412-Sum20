da81214f22ce7b9aa7ac5690e714aaf55852fe01
Keshav Maheshwari <km02@bu.edu>
2020-05-29 00:07:52 -0400

    done p2

---
+++ PS1/PS1.P2.js
@@ -0,0 +1,17 @@
+const evaluate = expression => {
+    const firstDigit = parseInt(expression[0])
+    const lastDigit = parseInt(expression[2])
+    const operator = expression[1];
+
+    switch(operator) {
+        case '+': return firstDigit+lastDigit;
+        case '-': return firstDigit-lastDigit;
+        case '*': return firstDigit*lastDigit;
+        case '/': return firstDigit/lastDigit;
+    }
+}
+
+console.log(`4+2 = ${evaluate('4+2')}`);
+console.log(`5*7 = ${evaluate('5*7')}`);
+console.log(`6-1 = ${evaluate('6-1')}`);
+console.log(`9/2 = ${evaluate('9/2')}`);
