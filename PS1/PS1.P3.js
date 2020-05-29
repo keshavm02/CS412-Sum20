801665ba4eb2acd642f4322d84fc3d13a002d163
Keshav Maheshwari <km02@bu.edu>
2020-05-29 16:51:14 -0400

    added colons to p3

--- PS1/PS1.P3.js
+++ PS1/PS1.P3.js
@@ -3,15 +3,10 @@ const function3 = (str, func) => func(str);
 const expression1 = function3("supercalifragilisticexpialidocious", function separator(s) {
     let splitList = s.replace(/c/g, '.c').split('.');
 
-    // let i = null;
-    // for (i=1;i<splitList.length;i++) {
-    //     splitList[i] = 'c'+splitList[i];
-    // }
-
     return splitList;
 });
 
-console.log(expression1)
+console.log(expression1);
 
 const expression2 = function3("supercalifragilisticexpialidocious", function capitalizeA(s) {
     const modifiedString = s.replace(/a/g, 'A');
@@ -19,12 +14,12 @@ const expression2 = function3("supercalifragilisticexpialidocious", function cap
     const length = s.length;
 
     const ret = {
-        originalString: s,
-        modifiedString: modifiedString,
-        numberReplaced: numberReplaced,
-        length: length
-    }
-    return ret
+        'originalString': s,
+        'modifiedString': modifiedString,
+        'numberReplaced': numberReplaced,
+        'length': length
+    };
+    return ret;
 });
 
 console.log(expression2);
