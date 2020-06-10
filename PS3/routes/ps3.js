30a6c0cd3955a3772da4d1be0163dc271623088a
Keshav Maheshwari <km02@bu.edu>
2020-06-10 11:44:09 -0400

    done 1b

--- PS3/routes/ps3.js
+++ PS3/routes/ps3.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('This is the ps3 route.');
+});
+
+router.get('/getString', function(req, res, next) {
+    res.render('getString', { string: 'Hello Prof.Donham/Katherine. Have a nice day!' });
 });
 
 module.exports = router;
