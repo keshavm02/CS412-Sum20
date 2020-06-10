005d5161d84839e98bbf53a27f4cc34e5429de4a
Keshav Maheshwari <km02@bu.edu>
2020-06-10 13:34:18 -0400

    removed comments

--- PS3/routes/ps3.js
+++ PS3/routes/ps3.js
@@ -10,19 +10,8 @@ router.get('/getString', function(req, res, next) {
 });
 
 router.route('/postString')
-    // .get((req, res, next) => {
-    //     res.render('index', {title: 'post String problem'})
-    // })
     .post((req, res, next) => {
         res.render('postString', { string: req.body.string, length: req.body.string.length});
-        // res.send(`string = ${req.body.string}`);
     })
-
-
-
-// router.post('/postString', function(req, res, next) {
-//     res.send(`firstName = ${req.body.firstName}`);
-//     // res.render('getString', { string: 'Hello Prof.Donham/Katherine. Have a nice day!' });
-// });
 
 module.exports = router;
