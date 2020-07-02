72eb9d3ac7cf405ab2e08ada2da97667c280c0be
Keshav Maheshwari <km02@bu.edu>
2020-07-01 22:22:31 -0400

    all working; all fixed

--- PS7/angular/src/app/parent/parent.component.ts
+++ PS7/angular/src/app/parent/parent.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WxServiceService as WxService} from '../wx-service.service';
 
 @Component({
   selector: 'app-parent',
-  template: '<app-form (eventEmitter)="getWeatherByCity($event)"></app-form>' + '<app-results [currentWeather]="currentWeather"></app-results>',
+  template: '<app-form (eventEmitter)="getWeatherByCity($event)"></app-form>' + '<app-results [currentWeather]="currentWeather" [cityName]="cityName" [cacheValue]="cacheValue" ></app-results>',
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
 
-  constructor(private wxService: WxService) { }
+  constructor(private wxService: WxService) {
+  }
+
   currentWeather: any;
+  cacheValue: any;
+  cityName: any;
 
   // tslint:disable-next-line:typedef
   getWeatherByCity(cityName: string) {
     this.wxService.getWeatherByCity(cityName).subscribe(
       response => {
         this.currentWeather = response['cityData']['main']['temp'];
+        this.cityName = response['cityData']['name'];
+        if (response['cached'] === true) {
+          this.cacheValue = 'cached';
+        } else {
+          this.cacheValue = 'not cached';
+        }
       }
     );
   }
--- PS7/angular/src/app/results/results.component.ts
+++ PS7/angular/src/app/results/results.component.ts
@@ -8,6 +8,8 @@ import {Component, OnInit, Input} from '@angular/core';
 export class ResultsComponent implements OnInit {
   // tslint:disable-next-line:no-input-rename
   @Input('currentWeather') currentWeather: any;
+  @Input('cacheValue') cacheValue: any;
+  @Input('cityName') cityName: any;
 
   constructor() {
   }
